1a6450eacd0ca20244e15635c166b5fec287eaae
paquesqueue <paweenwat.kwanja@gmail.com>
2026-05-15 23:00:08 +0700

    feature: merge test to the main project

--- BookManagementSystem/src/Global/Exceptions/NotFoundException.cs
+++ BookManagementSystem/src/Global/Exceptions/NotFoundException.cs
@@ -2,6 +2,8 @@ namespace Global.Exceptions;
 
 public class NotFoundException : Exception
 {
+    public int StatusCode { get; } = 404;
+
     public NotFoundException(string message) : base(message)
     {
     }
